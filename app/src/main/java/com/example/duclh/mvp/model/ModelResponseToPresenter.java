bb601837abcd52e7df414716ba27bbafd5accc6b
duclh <huynhduc96@gmail.com>
2017-07-24 17:10:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/duclh/mvp/model/ModelResponseToPresenter.java
@@ -0,0 +1,10 @@
+package com.example.duclh.mvp.model;
+
+/**
+ * Created by duclh on 24/07/2017.
+ */
+
+public interface ModelResponseToPresenter {
+    void onLoginSuccess();
+    void onLoginFailed();
+}
---
+++ app/src/main/java/com/example/duclh/mvp/view/ViewLoginListener.java
@@ -0,0 +1,10 @@
+package com.example.duclh.mvp.view;
+
+/**
+ * Created by duclh on 24/07/2017.
+ */
+
+public interface ViewLoginListener {
+    void onLoginViewSuccess();
+    void onLoginViewFailed();
+}
