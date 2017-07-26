8862286aa71d2871b1deb707432cd3ecc12376bc
duclh <huynhduc96@gmail.com>
2017-07-26 14:58:49 +0700

    data binding

--- app/src/main/java/com/example/duclh/mvp/TaskAdapter.java
+++ app/src/main/java/com/example/duclh/mvp/TaskAdapter.java
@@ -2,7 +2,10 @@ package com.example.duclh.mvp;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.duclh.mvp.data.model.Task;
+import com.example.duclh.mvp.databinding.ItemTaskBinding;
 
 import java.util.List;
 
@@ -41,35 +45,40 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_task, parent,
-                false);
-        v.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final int position = mRecyclerView.getChildAdapterPosition(view);
-                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                View dialogView =
-                        LayoutInflater.from(view.getContext()).inflate(R.layout.item_dialog, null);
-                final EditText editTitle = (EditText) dialogView.findViewById(R.id.edit_title);
-                final EditText editMessage = (EditText) dialogView.findViewById(R.id.edit_message);
-                editTitle.setText(mTaskList.get(position).getName());
-                editMessage.setText(mTaskList.get(position).getMessage());
-                builder.setView(dialogView)
-                        .setTitle("Update Task")
-                        .setPositiveButton("UPDATE", new DialogInterface.OnClickListener
-                                () {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                mPresenter
-                                        .updateTask(mTaskList, mTaskList.get(position).getId(), editTitle
-                                                .getText()
-                                                .toString(), editMessage.getText().toString());
-                            }
-                        }).setNegativeButton(R.string.action_cancel, null);
-                builder.create().show();
-            }
-        });
-        return new ViewHolder(v);
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ItemTaskBinding binding = DataBindingUtil.inflate(layoutInflater, R.layout.item_task, parent, false);
+        binding.setAdapter(this);
+        return new ViewHolder(binding);
+
+
+    }
+
+  public void onClickView(View view) {
+      Log.e("====>","dcmm");
+        final int position = mRecyclerView.getChildAdapterPosition(view);
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        View dialogView =
+                LayoutInflater.from(view.getContext()).inflate(R.layout.item_dialog, null);
+        final EditText editTitle = (EditText) dialogView.findViewById(R.id.edit_title);
+        final EditText editMessage = (EditText) dialogView.findViewById(R.id.edit_message);
+        editTitle.setText(mTaskList.get(position).getName());
+        editMessage.setText(mTaskList.get(position).getMessage());
+        builder.setView(dialogView)
+                .setTitle("Update Task")
+                .setPositiveButton("UPDATE", new DialogInterface.OnClickListener
+                        () {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        mPresenter
+                                .updateTask(mTaskList, mTaskList.get(position).getId(), editTitle
+                                        .getText()
+                                        .toString(), editMessage.getText().toString());
+                    }
+                }).setNegativeButton(R.string.action_cancel, null);
+        builder.create().show();
+
+
     }
 
     @Override
@@ -77,43 +86,44 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
         holder.bindData(mTaskList.get(position));
     }
 
+
+
     @Override
     public int getItemCount() {
         return mTaskList.isEmpty() ? 0 : mTaskList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private CheckBox mCheckFinish;
-        private TextView mTextTitle;
+    /** ViewHolder
+     *
+     *
+     */
 
-        public ViewHolder(View itemView) {
-            super(itemView);
-            mCheckFinish = (CheckBox) itemView.findViewById(R.id.check_finish);
-            mTextTitle = (TextView) itemView.findViewById(R.id.text_title);
-            itemView.findViewById(R.id.button_delete).setOnClickListener(this);
-            mCheckFinish.setOnClickListener(this);
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        private  ItemTaskBinding viewDataBinding;
+
+        public ViewHolder(ItemTaskBinding binding) {
+            super(binding.getRoot());
+            viewDataBinding = binding;
         }
+
 
         public void bindData(Task task) {
             if (task != null) {
-                mTextTitle.setText(task.getName());
-                mCheckFinish.setChecked(task.isFinish());
+                viewDataBinding.setTask(task);
+                viewDataBinding.setHolder(this);
             }
+        }
+        public void clickDelete(){
+               mPresenter.deleteTask(mTaskList, mTaskList.get(getAdapterPosition()).getId());
+        }
+        public void clickCheck(){
+            Log.e("clickCheck: ",viewDataBinding.checkFinish.isChecked()+"" );
+            boolean check = viewDataBinding.checkFinish.isChecked();
+            mPresenter.finishTask(mTaskList, mTaskList.get(getAdapterPosition()).getId(), check);
         }
 
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.button_delete:
-                    mPresenter.deleteTask(mTaskList, mTaskList.get(getAdapterPosition()).getId());
-                    break;
-                case R.id.check_finish:
-                    mPresenter.finishTask(mTaskList, mTaskList.get(getAdapterPosition()).getId(),
-                            mCheckFinish.isChecked());
-                    break;
-                default:
-                    break;
-            }
-        }
     }
 }
